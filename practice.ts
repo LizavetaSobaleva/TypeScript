00413d4c19ca67f1cb9fa37caba22bbaf1806da2
Lizaveta <sobaleva.lizaveta@gmail.com>
2023-07-20 20:09:39 +0200

    New commit

--- practice.ts
+++ practice.ts
@@ -27,3 +27,26 @@ export function greet(name: string): string {
 }
 //export const greet = (name: string): string => `Hello, ${name} how are you doing today?`
 console.log(greet('Liza'))
+
+//Now you have to write a function that takes an argument and returns the square of it.
+export const square = (num: number) : number => num ** 2
+console.log(square(12))
+
+//Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, 
+//and the negatives become positives.
+export function invert(array: number[]): number[] {
+    return array.map((n) => -n)
+}
+console.log(invert([-1, -2, 3]))
+
+//Implement a function which convert the given boolean value into its string representation.
+export const booleanToString = (b:boolean):string => String(b)
+console.log(typeof(booleanToString(true)))
+
+//Given a non-empty array of integers, return the result of multiplying the values together in order. 
+export const grow = (arr: number[]): number => arr.reduce((a, b) => a * b)
+console.log(grow([4, 1, 1, 1, 4]))
+
+//Create a method to see whether the string is ALL CAPS.
+export const isUpperCase = (str: string) => str === str.toUpperCase()
+console.log(isUpperCase('HELLO I AM DONALD'))
